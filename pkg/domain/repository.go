fc29c5aa0267faf74aadec2693e634d178503ef7
Perepechko Aleksey <elf.ast.ahe@gmail.com>
2026-03-09 21:17:27 +0300

    fixes

--- pkg/domain/repository.go
+++ pkg/domain/repository.go
@@ -17,7 +17,7 @@ type CrudRepository[T Entity[ID], ID any] interface {
 }
 
 // OwnedRepository repository with simple crud methods for owned instances
-type OwnedRepository[T Entity[T], ID any, OwnerID comparable] interface {
+type OwnedRepository[T Entity[ID], ID any, OwnerID comparable] interface {
 	Find(ctx context.Context, ownerID OwnerID, id ID) (T, error)
 
 	List(ctx context.Context, ownerID OwnerID, limit, offset int) ([]T, error)
