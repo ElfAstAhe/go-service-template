1c209849ae2f79aee4f534354ea45dd2459b5b96
Perepechko Aleksey <elf.ast.ahe@gmail.com>
2026-05-28 08:50:33 +0300

    add gRPC runner

--- internal/app/container/grpc.go
+++ internal/app/container/grpc.go
@@ -2,8 +2,15 @@ package container
 
 import (
 	"context"
+	"errors"
 
 	"github.com/ElfAstAhe/go-service-template/pkg/container"
+	"github.com/ElfAstAhe/go-service-template/pkg/errs"
+)
+
+const (
+	InstanceGRPCService string = "grpcService"
+	InstanceGRPCRunner  string = "grpcRunner"
 )
 
 type GRPCContainer struct {
@@ -20,5 +27,13 @@ func NewGRPCContainer(orchestrator container.Orchestrator) *GRPCContainer {
 }
 
 func (gc *GRPCContainer) Init(initCtx context.Context) error {
+	err := errors.Join(
+		gc.RegisterProvider(InstanceGRPCService, gc.providerGRPCService),
+		gc.RegisterProvider(InstanceGRPCRunner, gc.providerGRPCRunner),
+	)
+	if err != nil {
+		return errs.NewContainerError(gc.GetName(), "container init: register providers failed", err)
+	}
+
 	return nil
 }
