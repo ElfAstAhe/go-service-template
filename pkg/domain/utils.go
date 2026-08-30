30237a57d869b3230aa1cd998f647690879fff2e
elf.ast.ahe <elf.ast.ahe@gmail.com>
2026-08-30 16:13:19 +0300

    few improvements

--- pkg/domain/utils.go
+++ pkg/domain/utils.go
@@ -1,5 +1,10 @@
 package domain
 
+import (
+	"github.com/ElfAstAhe/go-service-template/pkg/errs"
+	"github.com/google/uuid"
+)
+
 func EntitiesToIDList[T Entity[ID], ID comparable](src []T) []ID {
 	res := make([]ID, len(src))
 	if len(res) == 0 {
@@ -10,4 +15,15 @@ func EntitiesToIDList[T Entity[ID], ID comparable](src []T) []ID {
 	}
 
 	return res
+}
+
+func AssignUUIDv7[T Entity[string]](entity T) error {
+	newID, err := uuid.NewV7()
+	if err != nil {
+		return errs.NewBllError("AssignUUIDv7", "generate new id", err)
+	}
+
+	entity.SetID(newID.String())
+
+	return nil
 }
